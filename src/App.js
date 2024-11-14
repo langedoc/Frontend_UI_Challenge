e5a1b81375d79ef7841aed362be822976716007c
yulia <langedoc@gmail.com>
2024-11-14 15:57:00 +0100

    sass integration

--- src/App.js
+++ src/App.js
@@ -1,1 +1,7 @@
- 
+import { hot } from 'react-hot-loader/root';
+
+const App = () => {
+  // Your app code
+};
+
+export default hot(App); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,47 @@
- 
+const path = require('path');
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                'react-refresh/babel'
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin()
+  ],
+  devServer: {
+    hot: true,
+    contentBase: './public',
+    port: 3000
+  }
+}; 
