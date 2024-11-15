705443bc038effba82647e2260274fc3367da568
yulia <langedoc@gmail.com>
2024-11-15 18:33:26 +0100

    style: ItemCard.scss

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import placeholder from '../../assets/coming_soon.png';
-
+import './ItemCard.scss';
 export default function ItemCard({movie}) {
     return (
         <div className="item-card">
-            <h3 className="item-card-title">{movie.title}</h3>
             <img
-                className="item-card-img" 
                 src={
                     movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : placeholder
                 }
                 alt="movie poster"
             />
+            <h3>{movie.title}</h3>
         </div>
     );
 };
