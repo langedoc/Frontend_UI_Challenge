3a700e608c78b7679bcb05d4544ebafd6b177939
yulia <langedoc@gmail.com>
2024-11-16 17:55:14 +0100

    feat: header component created and added to layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>MyMovie</h1>
+            <button>MY WISHLIST</button>
+        </header>
+    );
+}
