0b9d0726948d81414bce0ec708e97673fa091deb
yulia <langedoc@gmail.com>
2024-11-16 12:25:47 +0100

    feat: router basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import {MovieProvider} from './contexts/MovieContext';
-// import Home from './pages/Home/Home';
+import Home from './pages/Home/Home';
 import Detailed from './pages/Detailed/Detailed';
 import './styles/main.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/detailed",
+    element: <Detailed />,
+  },
+]);
 
 const App = () => {
   
   return (
     <MovieProvider>
-      {/* <Home /> */}
-      <Detailed/>
+      <RouterProvider router={router} />
     </MovieProvider>
   );
 };
