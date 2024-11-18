d797e21ad4ecc0c3297994fd4e9bfe11c6897608
yulia <langedoc@gmail.com>
2024-11-18 11:23:34 +0100

    feat: WishlistOverlay .jsx and .scss files created

---
+++ src/components/WishlistOverlay/WishlistOverlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './WishlistOverlay.scss';
+
+export default function WishlistOverlay() {
+
+    return (
+        <div className="wishlist-overlay">
+            <h3>Wishlist</h3>
+        </div>
+    );
+};
