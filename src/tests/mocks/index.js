24f2dd4b005bd2b1d0272d67e51ac63ab421827e
yulia <langedoc@gmail.com>
2024-11-19 10:35:13 +0100

    test: mock data

---
+++ src/tests/mocks/index.js
@@ -0,0 +1,9 @@
+import { popularMoviesData } from "./popularMoviesData";
+import { upcomingMoviesData } from "./upcomingMoviesData";
+import { topRatedMoviesData } from "./topRatedMoviesData";
+
+export const mocks = {
+    popularMoviesData,
+    upcomingMoviesData,
+    topRatedMoviesData
+}
